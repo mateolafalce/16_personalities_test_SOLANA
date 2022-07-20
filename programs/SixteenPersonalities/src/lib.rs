e1bf6fefcdb30e981e73fa2c08c21fbf6775ae9e
Mateo Lafalce <mateolafalce3@gmail.com>
2022-07-20 12:38:37 -0300

    20/7/2022

--- programs/SixteenPersonalities/src/lib.rs
+++ programs/SixteenPersonalities/src/lib.rs
@@ -1,12 +1,21 @@
 use anchor_lang::prelude::*;
 
-declare_id!("2sZ2p39s4wTQrVteV8XP5r8d8cbqr2pdQnpvU3BvcbkK");
+declare_id!("oYt6UFzxCZLxweeFg9DQXipYjskuw3mANRbvBMaLNUa");
 
 #[program]
 pub mod sixteen_personalities {
     use super::*;
 
+    pub fn create_quiz(ctx: Context<Create>, 
+        authority: Pubkey
+    ) -> Result<()> {
+        let answer = &mut ctx.accounts.answer;
+        answer.authority = authority;
+        Ok(())
+    }
+
     pub fn initialize(ctx: Context<Answers>,
+        authority: Pubkey,
         i_am_a_worrier1: bool,
         i_make_friends_easily2: bool,
         i_have_a_lot_of_imagination3: bool,
@@ -15,8 +24,18 @@ pub mod sixteen_personalities {
         i_get_angry_easily6: bool,
         i_really_enjoy_large_parties_and_gatherings7: bool,
         i_think_at_is_important8: bool,
-        i_sometimes_deceive_others_to_get_my_own_way9: bool
-        i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10: bool
+        i_sometimes_deceive_others_to_get_my_own_way9: bool,
+        i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10: bool,
+        i_often_feel_sad11: bool,
+        i_like_to_take_charge_of_situations_and_events12: bool,
+        i_experience_deep_and_varied_emotions13:bool,
+        i_love_to_help_others14: bool,
+        i_always_keep_my_promises15: bool,
+        i_find_it_difficult_to_approach_others16: bool,
+        i_am_always_busy_always_on_the_go17: bool,
+        i_prefer_variety_to_routine18: bool,
+        i_love_a_good_argument_a_good_fight19: bool,
+        i_work_very_hard20: bool
     ) -> Result<()> {
         let answer = &mut ctx.accounts.answer;
         answer.i_am_a_worrier1 = i_am_a_worrier1; let i_am_a_worrier1_ = answer.i_am_a_worrier1 as u8;
@@ -29,7 +48,16 @@ pub mod sixteen_personalities {
         answer.i_think_at_is_important8 = i_think_at_is_important8; let i_think_at_is_important8_ = answer.i_think_at_is_important8 as u8;
         answer.i_sometimes_deceive_others_to_get_my_own_way9 = i_sometimes_deceive_others_to_get_my_own_way9; let i_sometimes_deceive_others_to_get_my_own_way9_ = answer.i_sometimes_deceive_others_to_get_my_own_way9 as u8;
         answer.i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10 = i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10; let i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10_ = answer.i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10 as u8;
-
+        answer.i_often_feel_sad11 = i_often_feel_sad11; let i_often_feel_sad11_ = answer.i_often_feel_sad11 as u8;
+        answer.i_like_to_take_charge_of_situations_and_events12 = i_like_to_take_charge_of_situations_and_events12; let i_like_to_take_charge_of_situations_and_events12_ = answer.i_like_to_take_charge_of_situations_and_events12 as u8;
+        answer.i_experience_deep_and_varied_emotions13 = i_experience_deep_and_varied_emotions13; let i_experience_deep_and_varied_emotions13_ = answer.i_experience_deep_and_varied_emotions13 as u8;
+        answer.i_love_to_help_others14 = i_love_to_help_others14; let i_love_to_help_others14_ = answer.i_love_to_help_others14 as u8;
+        answer.i_always_keep_my_promises15 = i_always_keep_my_promises15; let i_always_keep_my_promises15_ = answer.i_always_keep_my_promises15 as u8;
+        answer.i_find_it_difficult_to_approach_others16 = i_find_it_difficult_to_approach_others16; let i_find_it_difficult_to_approach_others16_ = answer.i_find_it_difficult_to_approach_others16 as u8;
+        answer.i_am_always_busy_always_on_the_go17 = i_am_always_busy_always_on_the_go17; let i_am_always_busy_always_on_the_go17_ = answer.i_am_always_busy_always_on_the_go17 as u8;
+        answer.i_prefer_variety_to_routine18 = i_prefer_variety_to_routine18; let i_prefer_variety_to_routine18_ = answer.i_prefer_variety_to_routine18 as u8;
+        answer.i_love_a_good_argument_a_good_fight19 = i_love_a_good_argument_a_good_fight19; let i_love_a_good_argument_a_good_fight19_ = answer.i_love_a_good_argument_a_good_fight19 as u8;
+        answer.i_work_very_hard20 = i_work_very_hard20; let i_work_very_hard20_ = answer.i_work_very_hard20 as u8;
 
         let score: u8 = i_am_a_worrier1_ 
         + i_make_friends_easily2_
@@ -39,8 +67,18 @@ pub mod sixteen_personalities {
         + i_get_angry_easily6_
         + i_really_enjoy_large_parties_and_gatherings7_
         + i_think_at_is_important8_
-        + i_sometimes_deceive_others_to_get_my_own_way9
-        + i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10;
+        + i_sometimes_deceive_others_to_get_my_own_way9_
+        + i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10_
+        + i_often_feel_sad11_
+        + i_like_to_take_charge_of_situations_and_events12_
+        + i_experience_deep_and_varied_emotions13_
+        + i_love_to_help_others14_
+        + i_always_keep_my_promises15_
+        + i_find_it_difficult_to_approach_others16_
+        + i_am_always_busy_always_on_the_go17_
+        + i_prefer_variety_to_routine18_
+        + i_love_a_good_argument_a_good_fight19_
+        + i_work_very_hard20_;
 
         /*if score <= 0 {
             msg!("Sos una persona timida timido");
@@ -48,12 +86,13 @@ pub mod sixteen_personalities {
             msg!("Sos una persona muy atrevida");
         }
         */
+        msg!("{}", score);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Answers<'info> {
+pub struct Create<'info> {
     #[account(init, payer = user, space = Answer16personalitites::LEN)]
     pub answer: Account<'info, Answer16personalitites>,
     #[account(mut)]
@@ -61,25 +100,45 @@ pub struct Answers<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Answers<'info> {
+    #[account(mut, has_one = authority)]
+    pub answer: Account<'info, Answer16personalitites>,
+    pub authority: Signer<'info>,
+}
+
 #[account]
 pub struct Answer16personalitites {
-    i_am_a_worrier1: bool,
-    i_make_friends_easily2: bool,
-    i_have_a_lot_of_imagination3: bool,
-    i_trust_others4: bool,
-    i_complete_tasks_successfully5: bool,
-    i_get_angry_easily6: bool,
-    i_really_enjoy_large_parties_and_gatherings7: bool,
-    i_think_at_is_important8: bool,
-    i_sometimes_deceive_others_to_get_my_own_way9: bool,
-    i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10: bool
+    pub authority: Pubkey,
+    pub i_am_a_worrier1: bool,
+    pub i_make_friends_easily2: bool,
+    pub i_have_a_lot_of_imagination3: bool,
+    pub i_trust_others4: bool,
+    pub i_complete_tasks_successfully5: bool,
+    pub i_get_angry_easily6: bool,
+    pub i_really_enjoy_large_parties_and_gatherings7: bool,
+    pub i_think_at_is_important8: bool,
+    pub i_sometimes_deceive_others_to_get_my_own_way9: bool,
+    pub i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10: bool,
+    pub i_often_feel_sad11: bool,
+    pub i_like_to_take_charge_of_situations_and_events12: bool,
+    pub i_experience_deep_and_varied_emotions13: bool,
+    pub i_love_to_help_others14: bool,
+    pub i_always_keep_my_promises15: bool,
+    pub i_find_it_difficult_to_approach_others16: bool,
+    pub i_am_always_busy_always_on_the_go17: bool,
+    pub i_prefer_variety_to_routine18: bool,
+    pub i_love_a_good_argument_a_good_fight19: bool,
+    pub i_work_very_hard20: bool
 
 }
 
 impl Answer16personalitites {
     const LEN: usize = DISCRIMINATOR 
-    + BOOL_ANSWERS;
+    + BOOL_ANSWERS 
+    + PUBKEY;
 }
 
 const DISCRIMINATOR: usize = 8;
-const BOOL_ANSWERS: usize = 1 * 10;
+const BOOL_ANSWERS: usize = 1 * 20;
+const PUBKEY: usize = 32;
--- tests/SixteenPersonalities.ts
+++ tests/SixteenPersonalities.ts
@@ -1,14 +1,31 @@
 import * as anchor from "@project-serum/anchor";
 const { SystemProgram } = anchor.web3;
 
-describe("SixteenPersonalities", () => {
+describe("Sixteen Personalities Test", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
   const program = anchor.workspace.SixteenPersonalities;
   const answer = anchor.web3.Keypair.generate();
 
+
+  it("Creating account", async () => {
+    const tx = await program.rpc.createQuiz(
+      provider.wallet.publicKey,
+    {
+      accounts: {
+        answer: answer.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [answer],
+    });
+    console.log("Your PDA has been created, check the signature", tx);
+  });
+
+
   it("Testing your personality", async () => {
-    const tx = await program.rpc.initialize(
+    const txe = await program.rpc.initialize(
+      provider.wallet.publicKey,
       false, //i_worry_about_everything1 ?
       false, //i_make_friends_easily2 ?
       true, //i_have_a_lot_of_imagination3 ?
@@ -19,16 +36,23 @@ describe("SixteenPersonalities", () => {
       true, //i_think_at_is_important8 ?
       false, //i_sometimes_deceive_others_to_get_my_own_way9 ?
       true, //i_dont_like_things_to_be_a_mess_i_like_to_tidy_up10 ?
+      false, //i_often_feel_sad11 ?
+      false, //i_like_to_take_charge_of_situations_and_events12 ?
+      true, //i_experience_deep_and_varied_emotions13 ?
+      false, //i_love_to_help_others14 ?
+      true, //i_always_keep_my_promises15 ?
+      false, //i_find_it_difficult_to_approach_others16 ?
+      true, //i_am_always_busy_always_on_the_go17 ?
+      false, //i_prefer_variety_to_routine18 ?
+      true, //i_love_a_good_argument_a_good_fight19 ?
+      true, //i_work_very_hard20 ?
     {
       accounts: {
         answer: answer.publicKey,
-        user: provider.wallet.publicKey,
-        systemProgram: SystemProgram.programId,
-      },
-      signers: [answer],
+        authority: provider.wallet.publicKey,
+      }
     });
-
-
-    console.log("Your transaction signature", tx);
+    console.log("The result of your personality test is here:", txe);
   });
+
 });
